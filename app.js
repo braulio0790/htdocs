a158f9bbb2a38ba5bfad65ac45af509c9e61d966
braulio0790 <braulio90.lds@gmail.com>
2023-05-07 00:12:10 -0600

    M13

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-//express web server
+//Required packages
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./mongoDB/conectionDB');
-const path = require('path');
-
+const data = require('./controllers/professional');
+//const path = require('path');
+//Set up the port
 const port = process.env.PORT || 8080;
+//call express int the variable app
 const app = express();
 //Allow access to all the files in the frontend
 app.use(express.static('frontend'));
+//app.use(express.static('controllers'));
+
 
 app
 .use(bodyParser.json())
@@ -20,7 +24,12 @@ app
 app.get('/frontend', (req, res) => {
     res.sendFile(__dirname + '/frontend/index.html')
  });
+//Retunr a JSON object
+app.get('/professional', (req, res) =>{
+    res.json(data);
+});
 
+//Test the connection to MongoDB is working 
 mongodb.initDb((err,mongodb) =>{
     if (err) {
         console.log(err);
