1f27f370dbfbab9bfaa9b68e4bc861990e8d6606
braulio0790 <braulio90.lds@gmail.com>
2023-05-09 01:33:27 -0600

    C19

--- controllers/professional.js
+++ controllers/professional.js
@@ -4,10 +4,13 @@ const mongodb = require('../mongoDB/conectionDB');
 //This code connect the DB, request info from the collection, loop it, construct a JSON , give a good response, and export function
 
 const getData = async (req,res,next) => {
-  console.log('accessing get data');  
+  console.log('accessing get data');
   const result = await mongodb.getDb().db().collection('professional').find();
+  console.log('Result content is ' + result);
     await result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
+      console.log('lists content is ' + lists); 
+      console.log('lists[0] content is ' + lists[0]); 
       res.status(200).json(lists[0]);
     });
 };
